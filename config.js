39334aa9fd7488dac0e320ff1e1fdcb5b76e8f31
viktor1944 <65153389+viktor1944@users.noreply.github.com>
2026-07-31 12:54:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // CONFIGURAÇÃO DO SITE — BOMBACHA FERRAMENTAS
 window.BOMBACHA_CONFIG = {
-  DOWNLOAD_URL: "https://github.com/viktor1944/bombacha-ferramentas/releases/latest/download/Bombacha_Ferramentas_Recorta_Facil_Setup_v1.0.5.3_Offline_MP3_Corrigido.exe",
+  DOWNLOAD_URL: "https://github.com/viktor1944/bombacha-ferramentas/releases/download/v1.0.5.3/Bombacha_Ferramentas_Recorta_Facil_Setup_v1.0.6.2_Offline_MP4_MP3_Player_Corrigido.1.exe",
   VERSION: "1.0.4",
   FILE_SIZE: "71.5 MB",
   SHA256: "0a4b5fcd0926fb41badaf75497b54b55591d3f4d8c17fa46dd4eaa4cf8d3f86f",
